0f37e43929811c7adb8e042c2c6ac720b371ce4d
Fernando Guevara <luisfernandoguevara95@gmail.com>
2018-07-12 21:27:06 -0500

    Add field verification

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 // Instanciar ambas clases
 const quoting = new Quoting();
 const ui = new UI();
+
+// Obtener el formulario
+const form = document.getElementById('form');
+// Event Listener cuando se envia el formulario
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Leer moneda selecionada
+    const coinSelect = document.getElementById('coin');
+    const coinSelected = coinSelect.options[coinSelect.selectedIndex].value;
+
+    // Leer criptomoneda selecionada
+    const cryptocurrencySelect = document.getElementById("cryptocurrency");
+    const cryptocurrencySelected = cryptocurrencySelect.options[cryptocurrencySelect.selectedIndex].value;
+
+    // Comprobar que ambos campos tengan datos
+    if (coinSelected === '' || cryptocurrencySelected === '') {
+        console.log('Faltan datos');
+    } else {
+        console.log('Cotizar');
+    }
+})
--- js/ui.js
+++ js/ui.js
@@ -10,8 +10,19 @@ class UI {
 
     buildSelect() {
         quoting.getCoinsAPI()
-            .then(data => {
-                console.log(data);
+            .then(coins => {
+                // Crea un select con las opciones
+                const arrayCoins = coins.coins.data;
+                const select = document.getElementById("cryptocurrency");
+
+                // Construir SELECT desde la REST API
+                arrayCoins.forEach(coin => {
+                    // Añadir el ID y el valor, despues asignarlo al select
+                    const option = document.createElement('option');
+                    option.value = coin.id;
+                    option.appendChild(document.createTextNode(coin.name));
+                    select.appendChild(option);
+                })
             })
     }
 }
